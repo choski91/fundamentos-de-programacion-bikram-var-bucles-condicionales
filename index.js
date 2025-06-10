787fbde768cc394ac1badb630e8d520ebee49c42
choski91 <mlaura.smichowski@gmail.com>
2025-06-10 16:31:48 +0200

    ultimos cambios

--- index.js
+++ index.js
@@ -1,21 +1,30 @@
-let variableSinValor;
+ //ejercicio 1
+ let variableSinValor;
 
-let booleano1 = true;
-let booleano2 = false;
+ //ejercicio 2
+ let booleano1 = true;
+ let booleano2 = false;
 
-const PI = 3.14;
+ //ejercicio 3
+ const PI = 3.14;
 
-const TAU = 2* PI;
+ //ejercicio 4
+ const TAU = 2* PI;
 
-let booleanoAnd = booleano1 && booleano2;
+ //ejercicio 5
+ let booleanoAnd = booleano1 && booleano2;
 
-let booleanoNot = !booleano1;
+ //ejercicio 6
+ let booleanoNot = !booleano1;
 
-let booleanoMix0 = booleano1 + booleano2 && booleano1;
+ //ejercicio 7
+ let booleanoMix0 = booleano1 + booleano2 && booleano1;
 
-let incrementarDesp = 2;
-let resultadoDesp = incrementarDesp ++;
+ //ejercicio 8
+ let incrementarDesp = 2;
+ let resultadoDesp = incrementarDesp ++;
 
+ //ejercicio 9
  let incrementarAntes = 2;
  let resultadoAntes = ++ incrementarAntes ;
 
@@ -36,8 +45,11 @@ let resultadoDesp = incrementarDesp ++;
  //ejercicio 12
  let sumaPares = 0;
  for(i = 0; i < 10; i++) {
-    i++
- }
+    if (i % 2 == 0) {
+      sumaPares += i;
+    }
+
+    } 
 
  //ejercicio 13
  let variableValorNumerico = 5;
@@ -49,7 +61,51 @@ let resultadoDesp = incrementarDesp ++;
  const MiNumeroFav = 3;
  
  //ejercicio 16
- 
- //ejercicio 17
+ let  booleanoOr = booleano1 || booleano2;
 
+ //ejercicio 17
  let booleanoMix1 = (booleano1 && (TAU / 2 === PI)) || (variableValorNumerico >= MiNumeroFav);
+
+ //ejercicio 18
+ let seisNoEsNueve = booleano1 !== booleano2;
+
+ //ejercicio 19
+ let booleanoMix2 = (variableValorNumerico > 0) || (-(MiNumeroFav * TAU));
+
+ //ejercicio 20
+ let valorSuma = MiNumeroFav + variableValorNumerico;
+
+ //ejercicio 21
+ let valorResta = MiNumeroFav - variableValorNumerico;
+
+ //ejercicio 22
+ let valorMultiplicacion = MiNumeroFav * variableValorNumerico;
+
+ //ejercicio 23
+ let valorDivision = MiNumeroFav / 3;
+
+ //ejercicio 24
+ let contarHasta10 = 0;
+   while ( contarHasta10 < 10) {
+    contarHasta10++
+  }
+
+ //ejercicio 25
+ let preI = 0;
+ let preJ = 0;
+  for (let i = 0; i < 11; i++) {
+   preI = preI + ++preJ;
+   
+};
+
+
+ //ejercicio 26
+ let sumaImpares = 0;
+ for ( let i = 0; i < 10; i++ ) {
+ if ( i % 2 == 1){ 
+   sumaImpares += i;
+ }
+ }
+
+
+
