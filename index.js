b1f174526e8c564ca56502be062fcc16ef823517
choski91 <mlaura.smichowski@gmail.com>
2025-06-10 12:21:00 +0200

    cambios lunes

---
+++ index.js
@@ -0,0 +1,55 @@
+let variableSinValor;
+
+let booleano1 = true;
+let booleano2 = false;
+
+const PI = 3.14;
+
+const TAU = 2* PI;
+
+let booleanoAnd = booleano1 && booleano2;
+
+let booleanoNot = !booleano1;
+
+let booleanoMix0 = booleano1 + booleano2 && booleano1;
+
+let incrementarDesp = 2;
+let resultadoDesp = incrementarDesp ++;
+
+ let incrementarAntes = 2;
+ let resultadoAntes = ++ incrementarAntes ;
+
+ //ejercicio 10
+ let contarHasta10_2 = 0;
+
+ for (let i= 0; i < 10; i++) {
+ contarHasta10_2++;
+ }
+    
+ //ejercicio 11   
+ let postI = 0;
+ let postJ = 0;
+
+ for ( i = 0; i < 11; i++) {
+    postI = postI + postJ++;
+ } 
+ //ejercicio 12
+ let sumaPares = 0;
+ for(i = 0; i < 10; i++) {
+    i++
+ }
+
+ //ejercicio 13
+ let variableValorNumerico = 5;
+
+ //ejercicio 14
+ const  MiNombre = "Laura";
+
+ //ejercicio 15
+ const MiNumeroFav = 3;
+ 
+ //ejercicio 16
+ 
+ //ejercicio 17
+
+ let booleanoMix1 = (booleano1 && (TAU / 2 === PI)) || (variableValorNumerico >= MiNumeroFav);
